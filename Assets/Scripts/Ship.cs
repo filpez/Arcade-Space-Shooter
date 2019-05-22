b206592047a43f08a7b5d9496e1ca5d9e1a68a7c
filpez <joaofilipepereiracosta@hotmail.com>
2019-05-22 19:09:08 +0100

    Created Player's Ship and Shot

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter{
+    public class Ship : MonoBehaviour
+    {
+        public int lives = 3;
+        public static int max_lives = 3;
+
+        public int current_rockets = 3;
+        public int rockets_capacity = 3;
+        public static int max_rockets = 10;
+
+        public int current_shield = 40;
+        public int shield_capacity = 40;
+        public static int max_shield = 100;
+
+        public float shield_recharge_rate = 10;
+        public static float min_shield_recharge_rate = 3;
+        float lastHit = 0;
+
+    }
+
+
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter{
+    public class Shot : MonoBehaviour
+    {
+        public Ship shooter;
+
+        public int damage = 10;
+
+        public static float speed = 1500;
+
+        Rigidbody rb;
+
+        void Start() {
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * speed);
+        }
+    }
+}
+
